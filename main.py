517b6eb1f5f787a559f2338140ecbac5aa1e409c
Rob Clewley <4868709+robclewley@users.noreply.github.com>
2024-11-24 10:32:31 -0500

    main

---
+++ main.py
@@ -0,0 +1,52 @@
+# /// script
+# dependencies = [
+#     "pygame-ce",
+#     "python-i18n"
+# ]
+# ///
+import asyncio
+import pygame
+import pygame_gui
+import i18n
+
+from pygame_gui.core import ObjectID
+
+
+async def main():
+    gui_dims = (800, 600)
+    screen = pygame.display.set_mode(gui_dims, pygame.SRCALPHA)
+    pygame.font.init()
+    pygame.freetype.init()
+    pygame.display.init()
+
+    gui_manager = pygame_gui.UIManager(gui_dims,
+                            pygame_gui.PackageResource(package="fonts",
+                                                resource='ui_theme.json'))
+
+    text = pygame_gui.elements.UILabel(
+            pygame.Rect(50, 50, 250, 30),
+            "These are working TTF fonts!",
+            manager=gui_manager,
+            object_id=ObjectID("@test")
+    )
+
+    background = pygame.Surface(gui_dims)
+    background.fill("#333333")
+
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                break
+
+        dt = clock.tick(60) / 1000
+        screen.blit(background, (0, 0))
+        gui_manager.draw_ui(screen)
+
+        gui_manager.update(dt)
+        pygame.display.update()
+        await asyncio.sleep(0)
+    pygame.quit()
+
+asyncio.run(main())
